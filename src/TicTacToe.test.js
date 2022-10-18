9080e4384a960abffa4e46264f4e14815fff3215
Galih Pratama <hanamura.iost@gmail.com>
2022-10-18 14:10:15 +0700

    chore: remove test file

--- src/TicTacToe.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import TicTacToe from "./TicTacToe";
-
-test("renders learn react link", () => {
-  render(<TicTacToe />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
